955c335106c12bcf748248b8a562fe8ad4d93ed5
wsroda <wojciechsroda@gmail.com>
2019-12-03 23:14:37 +0100

    Added unity project, configured photon bolt, added players with movement and idle animations

---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string map)
+    {
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Bolt.EntityBehaviour<IPlayerCubeState>
+{
+    [SerializeField]
+    private float speed = 4f;
+
+    [SerializeField]
+    private Animator playerAnimator;
+
+    public float Speed { get => speed; set => speed = value; }
+
+    public Animator PlayerAnimator { get => playerAnimator; set => playerAnimator = value; }
+
+    public override void Attached()
+    {
+        state.SetTransforms(state.CubeTransform, transform);
+        state.SetAnimator(PlayerAnimator);
+    }
+
+    public override void SimulateOwner()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + movement * Speed * BoltNetwork.FrameDeltaTime;
+            state.IsMoving = true;
+        }
+        else
+        {
+            state.IsMoving = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (state.IsMoving == true)
+        {
+            state.Animator.Play("PlayerMoveAnimation");
+        }
+        else
+        {
+            state.Animator.Play("PlayerIdleAnimation");
+        }
+    }
+
+}
