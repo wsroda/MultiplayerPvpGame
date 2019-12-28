414156ae761a98cfd2bf212e21b0d79d16645a3e
bartekotto <30353652+bartekotto@users.noreply.github.com>
2019-12-28 18:16:59 +0100

    display scoreboards

--- Assets/Scripts/NetworkCallbacks.cs
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [BoltGlobalBehaviour]
 public class NetworkCallbacks : Bolt.GlobalEventListener
 {
+
+    public static List<IPvpPlayerState> playerStates = new List<IPvpPlayerState>();
+
     public override void SceneLoadLocalDone(string map)
     {
         // randomize a position
         var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
 
         // instantiate cube
-        BoltNetwork.Instantiate(BoltPrefabs.PlayerPrefab, spawnPosition, Quaternion.identity);
+        BoltEntity playerPrefab = BoltNetwork.Instantiate(BoltPrefabs.PlayerPrefab, spawnPosition, Quaternion.identity);
+        IPvpPlayerState playerState = playerPrefab.GetComponent<ChrControllerBolt>().state;
+        playerStates.Add(playerState);
     }
 }
---
+++ Assets/Scripts/displayScoreboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class displayScoreboard
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                refreshScoreboard();
+            }
+        }
+
+        private void refreshScoreboard()
+        {
+            
+        }
+    }
+}
